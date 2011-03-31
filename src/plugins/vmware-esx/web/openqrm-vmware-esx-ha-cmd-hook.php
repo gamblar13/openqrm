47b5a161b44e88c522de82734aed4002444f3461
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2011-03-31 21:55:33 +0000

    commit of openQRM 4.8, for a detailed changelog please check https://sourceforge.net/projects/openqrm/files/openQRM-4.8/
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2145 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- src/plugins/vmware-esx/web/openqrm-vmware-esx-ha-cmd-hook.php
+++ src/plugins/vmware-esx/web/openqrm-vmware-esx-ha-cmd-hook.php
@@ -2,19 +2,19 @@
 /*
   This file is part of openQRM.
 
-    openQRM is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License version 2
-    as published by the Free Software Foundation.
+	openQRM is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License version 2
+	as published by the Free Software Foundation.
 
-    openQRM is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	openQRM is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
+	You should have received a copy of the GNU General Public License
+	along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
 
-    Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+	Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
 */
 
 
@@ -52,25 +52,25 @@ function create_vmware_esx_vm($host_resource_id, $name, $mac, $memory, $cpu, $sw
 	global $OPENQRM_SERVER_IP_ADDRESS;
 	global $OPENQRM_EXEC_PORT;
 	global $RESOURCE_INFO_TABLE;
-    global $vmware_mac_address_space;
-    global $event;
+	global $vmware_mac_address_space;
+	global $event;
 	$event->log("create_vmware_esx_vm", $_SERVER['REQUEST_TIME'], 5, "vmware-esx-ha-hook", "Creating VMware ESX VM $name on Host $host_resource_ip", "", "", 0, 0, 0);
 	// start the vm on the host
 	$host_resource = new resource();
 	$host_resource->get_instance_by_id($host_resource_id);
-    // we need to have an openQRM server object too since some of the
-    // virtualization commands are sent from openQRM directly
-    $openqrm = new openqrm_server();
-    // send command to create vm
-    // also need to generate a new vmware mac for the first nic
-    $fn_mac_gen_res = new resource();
-    $fn_mac_gen_res->generate_mac();
-    $fn_suggested_mac = $fn_mac_gen_res->mac;
-    $fn_suggested_last_two_bytes = substr($fn_suggested_mac, 12);
-    $fn_mac = $vmware_mac_address_space.":".$fn_suggested_last_two_bytes;
-    $vm_create_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/vmware-esx/bin/openqrm-vmware-esx create -i ".$host_resource->ip." -n ".$name." -m ".$fn_mac." -r ".$memory." -c ".$cpu." -s ".$swap." ".$additional_nic_str;
+	// we need to have an openQRM server object too since some of the
+	// virtualization commands are sent from openQRM directly
+	$openqrm = new openqrm_server();
+	// send command to create vm
+	// also need to generate a new vmware mac for the first nic
+	$fn_mac_gen_res = new resource();
+	$fn_mac_gen_res->generate_mac();
+	$fn_suggested_mac = $fn_mac_gen_res->mac;
+	$fn_suggested_last_two_bytes = substr($fn_suggested_mac, 12);
+	$fn_mac = $vmware_mac_address_space.":".$fn_suggested_last_two_bytes;
+	$vm_create_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/vmware-esx/bin/openqrm-vmware-esx create -i ".$host_resource->ip." -n ".$name." -m ".$fn_mac." -r ".$memory." -c ".$cpu." -s ".$swap." ".$additional_nic_str;
 	$event->log("create_vmware_esx_vm", $_SERVER['REQUEST_TIME'], 5, "vmware-esx-ha-hook", "Running $vm_create_cmd", "", "", 0, 0, 0);
-    $openqrm->send_command($vm_create_cmd);
+	$openqrm->send_command($vm_create_cmd);
 }
 
 
@@ -84,16 +84,16 @@ function fence_vmware_esx_vm($host_resource_id, $mac) {
 	global $event;
 
 	// fences the vm on its host
-    $host_resource = new resource();
-    $host_resource->get_instance_by_id($host_resource_id);
+	$host_resource = new resource();
+	$host_resource->get_instance_by_id($host_resource_id);
 	$event->log("fence_vmware_esx_vm", $_SERVER['REQUEST_TIME'], 5, "vmware-esx-ha-hook", "Fencing VMware ESX VM $mac from Host $host_resource_id", "", "", 0, 0, 0);
-    // we need to have an openQRM server object too since some of the
-    // virtualization commands are sent from openQRM directly
-    $openqrm = new openqrm_server();
-    // send command to fence the vm on the host
-    $vm_fence_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/vmware-esx/bin/openqrm-vmware-esx fence -i ".$host_resource->ip." -m ".$mac;
+	// we need to have an openQRM server object too since some of the
+	// virtualization commands are sent from openQRM directly
+	$openqrm = new openqrm_server();
+	// send command to fence the vm on the host
+	$vm_fence_cmd = "$OPENQRM_SERVER_BASE_DIR/openqrm/plugins/vmware-esx/bin/openqrm-vmware-esx fence -i ".$host_resource->ip." -m ".$mac;
 	$event->log("fence_vmware_esx_vm", $_SERVER['REQUEST_TIME'], 5, "vmware-esx-ha-hook", "Running $vm_fence_cmd", "", "", 0, 0, 0);
-    $openqrm->send_command($vm_fence_cmd);
+	$openqrm->send_command($vm_fence_cmd);
 }
 
 
