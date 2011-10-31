f5e66e35a4d6ca562a6f58077c2e7d80d0db2c24
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2011-10-31 12:43:13 +0000

    updated to openQRM 4.9
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2203 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- src/plugins/sanboot-storage/web/openqrm-sanboot-storage-resource-virtual-command-hook.php
+++ src/plugins/sanboot-storage/web/openqrm-sanboot-storage-resource-virtual-command-hook.php
@@ -14,7 +14,7 @@
 	You should have received a copy of the GNU General Public License
 	along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
 
-	Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+	Copyright 2011, openQRM Enterprise GmbH <info@openqrm-enterprise.com>
 */
 
 
@@ -61,7 +61,7 @@ function openqrm_sanboot_storage_resource_virtual_command($cmd, $resource_fields
 			break;
 		case "halt":
 			$event->log("openqrm_sanboot_storage_resource_virtual_command", $_SERVER['REQUEST_TIME'], 5, "openqrm-sanboot-storage-resource-virtual-command-hook.php", "Handling $cmd command", "", "", 0, 0, 0);
-			$virtual_command = "$OPENQRM_SERVER_BASE_DIR/openqrm/bin/dbclient -K 10 -y -i $OPENQRM_SERVER_BASE_DIR/openqrm/etc/dropbear/dropbear_rsa_host_key -p 22 root@$resource_ip 'shutdown.exe /f /t 2'";
+			$virtual_command = "$OPENQRM_SERVER_BASE_DIR/openqrm/bin/dbclient -K 10 -y -i $OPENQRM_SERVER_BASE_DIR/openqrm/etc/dropbear/dropbear_rsa_host_key -p 22 root@$resource_ip 'shutdown.exe /s /f /t 2'";
 			$openqrm_server->send_command($virtual_command);
 			sleep(2);
 			$openqrm_server->send_command($virtual_command);
