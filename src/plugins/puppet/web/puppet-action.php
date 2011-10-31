70b76ae3dac76949795c57d2a82d42b4852335a0
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2011-10-31 12:42:17 +0000

    updated to openQRM 4.9
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2202 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

--- src/plugins/puppet/web/puppet-action.php
+++ src/plugins/puppet/web/puppet-action.php
@@ -24,7 +24,7 @@ $puppet_domain = $_REQUEST["puppet_domain"];
 	You should have received a copy of the GNU General Public License
 	along with openQRM.  If not, see <http://www.gnu.org/licenses/>.
 
-	Copyright 2009, Matthias Rechenburg <matt@openqrm.com>
+	Copyright 2011, openQRM Enterprise GmbH <info@openqrm-enterprise.com>
 */
 
 
