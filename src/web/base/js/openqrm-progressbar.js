7cc0d50eb602d3b45516bd0c7a3e666375fecc1c
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2012-08-01 22:55:38 +0000

    update to openQRM Community 5.0
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2249 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

---
+++ src/web/base/js/openqrm-progressbar.js
@@ -0,0 +1,28 @@
+<script type="text/javascript">
+function trigger_progress_{id}() {
+	$("#progressbar_{id}").progressbar({});
+	$.ajax({
+		url: "{url}",
+		dataType: "text",
+		error: function(response) {
+			setTimeout("trigger_progress_{id}()", 1000);
+		},
+		success: function(response) {
+			var no = parseInt(response);
+			if (no < 0) { no = 0; }
+			$("#progressbar_{id}").progressbar("option", "value", no);
+			if (no < 100) {
+				$("#watcher_{id}").html("&nbsp;&nbsp;<small>" + response + "% - {lang_in_progress}</small>");
+				setTimeout("trigger_progress_{id}()", 1000);								
+			} else {
+				$("#watcher_{id}").html("&nbsp;<small>100 % - {lang_finished}</small>");
+			}	
+		}
+	});
+}
+trigger_progress_{id}();
+</script>
+<div class="progress_bar">
+	<div id="progressbar_{id}"></div>
+</div>
+<div id="watcher_{id}" class="progress_watcher">{lang_in_progress}</div>
