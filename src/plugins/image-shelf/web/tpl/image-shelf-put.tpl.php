02f901aeff77dfd5a7a79485e57eda771968ffbd
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2012-08-01 23:41:15 +0000

    update to openQRM Community 5.0
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2275 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

---
+++ src/plugins/image-shelf/web/tpl/image-shelf-put.tpl.php
@@ -0,0 +1,31 @@
+<!--
+/*
+    openQRM Enterprise developed by openQRM Enterprise GmbH.
+
+    All source code and content (c) Copyright 2012, openQRM Enterprise GmbH unless specifically noted otherwise.
+
+    This source code is released under the GNU General Public License version 2, unless otherwise agreed with openQRM Enterprise GmbH.
+    The latest version of this license can be found here: src/doc/LICENSE.txt
+
+    By using this software, you acknowledge having read this license and agree to be bound thereby.
+
+                http://openqrm-enterprise.com
+
+    Copyright 2012, openQRM Enterprise GmbH <info@openqrm-enterprise.com>
+*/
+-->
+<h1><img border=0 src="/openqrm/base/plugins/image-shelf/img/plugin.png"> Select openQRM Image (NFS or LVM-NFS)</h1>
+
+Please select one of following server images to put the template on
+
+<h4>Available NFS-Images to 'put' the template {image_template} on</h4>
+{image_shelf_nfs_table}
+
+<br>
+<br>
+<hr>
+<br>
+
+<h4>Available LVM-NFS-Images to 'put' the template {image_template} on</h4>
+{image_shelf_lvm_nfs_table}
+
