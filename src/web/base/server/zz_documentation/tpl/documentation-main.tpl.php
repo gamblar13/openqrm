3274c58b33f71dc4db3dfe990d89b8a6cb306ca2
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2012-08-01 23:08:55 +0000

    update to openQRM Community 5.0
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2260 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

---
+++ src/web/base/server/zz_documentation/tpl/documentation-main.tpl.php
@@ -0,0 +1,67 @@
+<!--
+/*
+    openQRM Enterprise developed by openQRM Enterprise GmbH.
+
+    All source code and content (c) Copyright 2012, openQRM Enterprise GmbH unless specifically noted otherwise.
+
+    This source code is released under the GNU General Public License version 2, unless otherwise agreed with openQRM Enterprise GmbH.
+    The latest version of this license can be found here: src/doc/LICENSE.txt
+
+    By using this software, you acknowledge having read this license and agree to be bound thereby.
+
+                http://openqrm-enterprise.com
+
+    Copyright 2012, openQRM Enterprise GmbH <info@openqrm-enterprise.com>
+*/
+-->
+<h1>{title}</h1>
+
+<link rel="stylesheet" href="{baseurl}/server/zz_documentation/css/documentation.css" type="text/css" media="all">
+
+
+<fieldset>
+	<legend>{technical}</legend>
+	<div style="float:left; width: 750px;">
+		{technical_description}
+		<br><br>
+		<a href="{technical_url}" target="_BLANK">{technical_url}</a>
+		<br><br>
+	</div>
+	<div class="floatbreaker" style="line-height:0px;height:0px;clear:both;">&nbsp;</div>
+</fieldset>
+
+<fieldset>
+	<legend>{howtos}</legend>
+	<div style="float:left; width: 900px;">
+		{howto1_title}
+		<br><br>
+		<a href="{howto1_url}" target="_BLANK">{howto1_url}</a>
+		<br><br>
+	</div>
+	<div style="float:left; width: 900px;">
+		{howto2_title}
+		<br><br>
+		<a href="{howto2_url}" target="_BLANK">{howto2_url}</a>
+		<br><br>
+	</div>
+	<div style="float:left; width: 900px;">
+		{howto3_title}
+		<br><br>
+		<a href="{howto3_url}" target="_BLANK">{howto3_url}</a>
+		<br><br>
+	</div>
+	<div class="floatbreaker" style="line-height:0px;height:0px;clear:both;">&nbsp;</div>
+</fieldset>
+
+<fieldset>
+	<legend>{api}</legend>
+	<div style="float:left; width: 750px;">
+		{api_description}
+		<br><br>
+		<a href="{api_url}" target="_BLANK">{api_url}</a>
+		<br><br>
+	</div>
+	<div class="floatbreaker" style="line-height:0px;height:0px;clear:both;">&nbsp;</div>
+</fieldset>
+
+
