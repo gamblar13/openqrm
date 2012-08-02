62b97ce2126d0ca006c4e74a6d24afecd587e0e3
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2012-08-02 00:03:37 +0000

    update to openQRM Community 5.0
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2281 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

---
+++ src/plugins/local-server/web/tpl/local-server-about-usage.tpl.php
@@ -0,0 +1,42 @@
+<!--
+/*
+    openQRM Enterprise developed by openQRM Enterprise GmbH.
+
+    All source code and content (c) Copyright 2012, openQRM Enterprise GmbH unless specifically noted otherwise.
+
+    This source code is released under the GNU General Public License version 2, unless otherwise agreed with openQRM Enterprise GmbH.
+    The latest version of this license can be found here: src/doc/LICENSE.txt
+
+    By using this software, you acknowledge having read this license and agree to be bound thereby.
+
+                http://openqrm-enterprise.com
+
+    Copyright 2012, openQRM Enterprise GmbH <info@openqrm-enterprise.com>
+*/
+//-->
+<h1><img border=0 src="{baseurl}/plugins/local-server/img/plugin.png"> {label}</h1>
+
+
+
+
+<div id="documentation_content" style="float:left;">
+
+
+<fieldset>
+	<legend>{usage_integrate_title}</legend>
+	<div id="introduction_content" style="float:left;">
+		{usage_integrate_content}
+	</div>
+</fieldset>
+
+<fieldset>
+	<legend>{usage_remove_title}</legend>
+	<div id="provides_content" style="float:left;">
+		{usage_remove_content}
+	</div>
+</fieldset>
+
+
+</div>
+
+
