fd1c0ebfa55cc61dd02d13c78f53eaf5071f761a
mattr_sf <mattr_sf@f7cd0d49-994a-0410-81fc-ecfcdc9cdbae>
2012-08-02 00:00:00 +0000

    update to openQRM Community 5.0
    git-svn-id: https://openqrm.svn.sourceforge.net/svnroot/openqrm/trunk@2279 f7cd0d49-994a-0410-81fc-ecfcdc9cdbae

---
+++ src/plugins/lcmc/web/lcmc-gui.php
@@ -0,0 +1,63 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+	"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>openQRM Server</title>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="content-script-type" content="text/javascript">
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<meta http-equiv="content-style-type" content="text/css">
+<meta http-equiv="content-script-type" content="text/javascript">
+<meta http-equiv="content-language" content="de">
+<meta name="date" content="2011-03-10T22:19:28+0100">
+<meta name="author" content="Alexander Kuballa">
+
+<link rel="stylesheet" media="all" href="css/htmlobject.css" type="text/css">
+<link rel="stylesheet" media="all" href="css/default.css" type="text/css">
+<link rel="stylesheet" href="css/menu.css" type="text/css">
+<link type="text/css" href="/openqrm/base/js/jquery/development-bundle/themes/smoothness/ui.all.css" rel="stylesheet">
+
+
+<script src="/openqrm/base/js/jquery/js/jquery-1.3.2.min.js" type="text/javascript"></script>
+<script src="/openqrm/base/js/jquery/js/jquery-ui-1.7.1.custom.min.js" type="text/javascript"></script>
+<script src="/openqrm/base/js/jqClock.min.js" type="text/javascript"></script>
+<script src="/openqrm/base/js/interface/interface.js" type="text/javascript"></script>
+<script src="js/menu.js" type="text/javascript"></script>
+
+
+</head>
+<body>
+
+
+<?php
+/*
+    openQRM Enterprise developed by openQRM Enterprise GmbH.
+
+    All source code and content (c) Copyright 2012, openQRM Enterprise GmbH unless specifically noted otherwise.
+
+    This source code is released under the GNU General Public License version 2, unless otherwise agreed with openQRM Enterprise GmbH.
+    The latest version of this license can be found here: src/doc/LICENSE.txt
+
+    By using this software, you acknowledge having read this license and agree to be bound thereby.
+
+                http://openqrm-enterprise.com
+
+    Copyright 2012, openQRM Enterprise GmbH <info@openqrm-enterprise.com>
+*/
+?>
+
+
+<applet archive="LCMC-applet-1.3.2.jar"
+	code="lcmc.LCMCApplet"
+	name=LCMCApplet
+	height="850"
+	width="1500"
+	vspace=0
+	hspace=0>
+	<param name="params" value="--no-plugin-check"/>
+</applet>
+
+
+</body>
+</html>
